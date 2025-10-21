389811c83c52badaa02600eca1ab7079698b2f6f
Sinewhales <whalessin12@gmail.com>
2025-10-21 17:22:15 +0200

    Farmer's Market
    - Added MoreJS
    - Villagers Now have a chance to trade the Farmer Market
    - Edited Farmer’s Market recipe to require an Heart of Emerald
    - Added Just Enough Professions and JEI Integrations
    - Added a Quest for the Farmer’s Market

---
+++ kubejs/server_scripts/farmer_market.js
@@ -0,0 +1,49 @@
+MoreJSEvents.villagerTrades(event => {
+    event.addTrade("minecraft:farmer", 1, Item.of("farmingforblockheads:market", 1), Item.of("minecraft:emerald", 2))
+})
+ServerEvents.recipes(event => {
+event.remove({ id: 'farmingforblockheads:market' })
+event.custom({
+  "type": "extendedcrafting:shaped_table",
+  "pattern": [
+    "AABAA",
+    "C   C",
+    "CDDDC",
+    "EFGFE",
+    "HFBFH"
+  ],
+  "key": {
+    "A": {
+      "item": "thermal:iron_plate"
+    },
+    "B": {
+      "item": "kubejs:heart_of_emerald"
+    },
+    "C": {
+      "item": "forestry:impregnated_stick"
+    },
+    "D": {
+      "item": "minecraft:red_wool"
+    },
+    "E": {
+      "item": "minecraft:emerald"
+    },
+    "F": {
+      "item": "quark:blossom_log"
+    },
+    "G": {
+      "type": "forge:nbt",
+      "item": "lootbags:loot_bag",
+      "count": 1,
+      "nbt": "{Color:13882323,Loot:\"lootbags:kjs/a93e9hs1ns87kfthowfa40cqh\",Name:\"Seed Bag\",Type:\"COMMON\"}"
+    },
+    "H": {
+      "item": "kubejs:impregnated_wood"
+    }
+  },
+  "result": {
+    "item": 'farmingforblockheads:market'
+  }
+})
+})
+
