94ecf1442f65b40b50aa072448fe205a2f4cdc79
Sinewhales <whalessin12@gmail.com>
2025-10-28 17:59:34 +0100

    Wood in Artisan Worktable is done

--- kubejs/server_scripts/crafting/wood_rework.js
+++ kubejs/server_scripts/crafting/wood_rework.js
@@ -462,4 +462,351 @@ event.remove({ id: "forestry:acacia_fireproof_planks" })
 event.remove({ id: "forestry:dark_oak_fireproof_planks" })
 event.remove({ id: "forestry:cherry_fireproof_planks" })
 
+// Integrated Dynamics
+// Menril
+event.remove({ id: "integrateddynamics:crafting/menril_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x integrateddynamics:menril_planks')
+.requireItemTag('#integrateddynamics:menril_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Solar Craft
+// Runic
+event.remove({ id: "solarcraft:runic_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x solarcraft:runic_planks')
+.requireItem('solarcraft:runic_log', "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Radiant
+event.remove({ id: "solarcraft:radiant_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x solarcraft:radiant_planks')
+.requireItem('solarcraft:radiant_log', "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Aether
+// Skyroot
+event.remove({ id: "aether:skyroot_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x aether:skyroot_planks')
+.requireItemTag('#aether:crafts_skyroot_planks', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Undergarden
+// Smogstem
+event.remove({ id: "undergarden:smogstem_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x undergarden:smogstem_planks')
+.requireItemTag('#undergarden:smogstem_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Wigglewood
+event.remove({ id: "undergarden:wigglewood_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x undergarden:wigglewood_planks')
+.requireItemTag('#undergarden:wigglewood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Wigglewood
+event.remove({ id: "undergarden:grongle_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x undergarden:grongle_planks')
+.requireItemTag('#undergarden:grongle_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Thermal
+// Rubberwood 
+event.remove({ id: "thermal:rubberwood_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x thermal:rubberwood_planks')
+.requireItemTag('#forge:rubberwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Botania
+// Livingwood
+event.remove({ id: "botania:livingwood_planks" })
+event.remove({ id: "botania:slab_recombine/livingwood_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x botania:livingwood_planks')
+.requireItemTag('#botania:livingwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Dreamwood
+event.remove({ id: "botania:dreamwood_planks" })
+event.remove({ id: "botania:slab_recombine/dreamwood_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x botania:dreamwood_planks')
+.requireItemTag('#botania:dreamwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Quark
+// Ashen
+event.remove({ id: "quark:world/crafting/woodsets/ancient/planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x quark:ancient_planks')
+.requireItemTag('#quark:ancient_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Azalea
+event.remove({ id: "quark:world/crafting/woodsets/azalea/planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x quark:azalea_planks')
+.requireItemTag('#quark:azalea_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Trumpet
+event.remove({ id: "quark:world/crafting/woodsets/blossom/planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x quark:blossom_planks')
+.requireItemTag('#quark:blossom_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Architect Palette
+// Twisted
+event.remove({ id: "architects_palette:twisted_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x architects_palette:twisted_planks')
+.requireItemTag('#architects_palette:twisted_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Twilight Forest
+// Twilight Oak
+event.remove({ id: "twilightforest:wood/twilight_oak_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x twilightforest:twilight_oak_planks')
+.requireItemTag('#twilightforest:twilight_oak_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Canopy
+event.remove({ id: "twilightforest:wood/canopy_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x twilightforest:canopy_planks')
+.requireItemTag('#twilightforest:canopy_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Mangrove
+event.remove({ id: "twilightforest:wood/mangrove_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x twilightforest:mangrove_planks')
+.requireItemTag('#twilightforest:mangrove_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Darkwood
+event.remove({ id: "twilightforest:wood/dark_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x twilightforest:dark_planks')
+.requireItemTag('#twilightforest:darkwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Timewood
+event.remove({ id: "twilightforest:wood/time_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x twilightforest:time_planks')
+.requireItemTag('#twilightforest:timewood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Transwood
+event.remove({ id: "twilightforest:wood/transformation_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x twilightforest:transformation_planks')
+.requireItemTag('#twilightforest:transwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Minewood
+event.remove({ id: "twilightforest:wood/mining_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x twilightforest:mining_planks')
+.requireItemTag('#twilightforest:mining_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Sortingwood
+event.remove({ id: "twilightforest:wood/sorting_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x twilightforest:sorting_planks')
+.requireItemTag('#twilightforest:sortwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Pale
+event.remove({ id: "palegardenbackport:pale_oak_planks_from_log" })
+event.remove({ id: "palegardenbackport:pale_oak_planks_from_stripped_wood" })
+event.remove({ id: "palegardenbackport:pale_oak_planks_from_stripped_log" })
+event.remove({ id: "palegardenbackport:pale_oak_planks_from_wood" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x palegardenbackport:pale_oak_planks')
+.requireItemTag('#palegardenbackport:pale_oak_log_blocks', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Traverse
+// Fir
+event.remove({ id: "traverse:fir_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", '2x traverse:fir_planks')
+.requireItemTag('#traverse:fir_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Jaden
+// Claret
+event.remove({ id: "netherexp:claret_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "netherexp:claret_planks")
+.requireItemTag('#netherexp:claret_stems', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Tropicraft
+// Mahogany
+event.remove({ id: "tropicraft:mahogany_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x tropicraft:mahogany_planks")
+.requireItem('tropicraft:mahogany_log', "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Palm
+event.remove({ id: "tropicraft:palm_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x tropicraft:palm_planks")
+.requireItem('tropicraft:palm_log', "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Mangrove
+event.remove({ id: "tropicraft:mangrove_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x tropicraft:mangrove_planks")
+.requireItemTag('#tropicraft:mangrove_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Terrestria
+// Cypress
+event.remove({ id: "terrestria:cypress_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:cypress_planks")
+.requireItemTag('#terrestria:cypress_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Hemlock
+event.remove({ id: "terrestria:hemlock_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:hemlock_planks")
+.requireItemTag('#terrestria:hemlock_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Japanese Maple
+event.remove({ id: "terrestria:japanese_maple_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:japanese_maple_planks")
+.requireItemTag('#terrestria:japanese_maple_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+
+// Rainbow Eucalyptus
+event.remove({ id: "terrestria:rainbow_eucalyptus_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:rainbow_eucalyptus_planks")
+.requireItemTag('#terrestria:rainbow_eucalyptus_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Redwood
+event.remove({ id: "terrestria:redwood_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:redwood_planks")
+.requireItemTag('#terrestria:redwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Rubber Wood
+event.remove({ id: "terrestria:rubber_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:rubber_planks")
+.requireItemTag('#terrestria:rubber_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Sakura
+event.remove({ id: "terrestria:sakura_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:sakura_planks")
+.requireItemTag('#terrestria:sakura_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Willow
+event.remove({ id: "terrestria:willow_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:willow_planks")
+.requireItemTag('#terrestria:willow_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Yucca
+event.remove({ id: "terrestria:yucca_palm_planks" })
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x terrestria:yucca_palm_planks")
+.requireItemTag('#terrestria:yucca_palm_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Hexerei
+// Mahogany
+event.remove({ id: "hexerei:mahogany_planks_from_stripped_wood" })
+event.remove({ id: "hexerei:mahogany_planks_from_stripped_log" })
+event.remove({ id: "hexerei:mahogany_planks" })
+event.remove({ id: "hexerei:mahogany_planks_from_wood" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x hexerei:mahogany_planks")
+.requireItemTag('#hexerei:mahogany_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Willow
+event.remove({ id: "hexerei:willow_planks_from_wood" })
+event.remove({ id: "hexerei:willow_planks_from_stripped_log" })
+event.remove({ id: "hexerei:willow_planks" })
+event.remove({ id: "hexerei:willow_planks_from_stripped_wood" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x hexerei:willow_planks")
+.requireItemTag('#hexerei:willow_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Witch Hazel
+event.remove({ id: "hexerei:witch_hazel_planks_from_stripped_wood" })
+event.remove({ id: "hexerei:witch_hazel_planks_from_wood" })
+event.remove({ id: "hexerei:witch_hazel_planks" })
+event.remove({ id: "hexerei:witch_hazel_planks_from_stripped_log" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x hexerei:witch_hazel_planks")
+.requireItemTag('#hexerei:witch_hazel_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Tinker
+// Greenheart
+event.remove({ id: "tconstruct:world/wood/greenheart/planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x tconstruct:greenheart_planks")
+.requireItemTag('#tconstruct:greenheart_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Skyroot
+event.remove({ id: "tconstruct:world/wood/skyroot/planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x tconstruct:skyroot_planks")
+.requireItemTag('#tconstruct:skyroot_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Bloodshroom
+event.remove({ id: "tconstruct:world/wood/bloodshroom/planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x tconstruct:bloodshroom_planks")
+.requireItemTag('#tconstruct:bloodshroom_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Enderbark
+event.remove({ id: "tconstruct:world/wood/enderbark/planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x tconstruct:enderbark_planks")
+.requireItemTag('#tconstruct:enderbark_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Ars
+event.remove({ id: "ars_nouveau:archwood_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x ars_nouveau:archwood_planks")
+.requireItemTag('#forge:logs/archwood', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Forbidden
+event.remove({ id: "forbidden_arcanus:fungyss_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x forbidden_arcanus:fungyss_planks")
+.requireItemTag('#forbidden_arcanus:fungyss_stems', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Aurum
+event.remove({ id: "forbidden_arcanus:aurum_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x forbidden_arcanus:aurum_planks")
+.requireItemTag('#forbidden_arcanus:mysterywood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Edelwood
+event.remove({ id: "forbidden_arcanus:edelwood_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x forbidden_arcanus:edelwood_planks")
+.requireItemTag('#forbidden_arcanus:edelwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Malum
+// Soulwood
+event.remove({ id: "malum:soulwood_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x malum:soulwood_planks")
+.requireItemTag('#malum:soulwood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Runewood
+event.remove({ id: "malum:runewood_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x malum:runewood_planks")
+.requireItemTag('#malum:runewood_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Environmental
+// Willow
+event.remove({ id: "environmental:willow_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x environmental:willow_planks")
+.requireItemTag('#environmental:willow_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Pine
+event.remove({ id: "environmental:pine_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x environmental:pine_planks")
+.requireItemTag('#environmental:pine_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Plum
+event.remove({ id: "environmental:plum_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x environmental:plum_planks")
+.requireItemTag('#environmental:plum_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// Wisteria
+event.remove({ id: "environmental:wisteria_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x environmental:wisteria_planks")
+.requireItemTag('#environmental:wisteria_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
+
+// End Phantasm
+event.remove({ id: "phantasm:pream_planks" })
+
+event.recipes.custommachinery.custom_craft("kubejs:artistan_worktable", "2x phantasm:pream_planks")
+.requireItemTag('#phantasm:pream_logs', 1, "input5").damageItemTag('#cb_microblock:tools/saw', 1, "tool")
 })
